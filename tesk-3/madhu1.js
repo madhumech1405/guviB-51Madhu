c8a3c08d426a30416a97a859e60a5f56ed94bc58
madhumech1405 <136167028+madhumech1405@users.noreply.github.com>
2023-08-25 23:09:02 +0530

    Update madhu1.js

--- tesk-3/madhu1.js
+++ tesk-3/madhu1.js
@@ -1,3 +1,4 @@
+
 let XLMHttpRequest=require('xhr2');
 
 let xhr=new XLMHttpRequest();
@@ -6,9 +7,12 @@ xhr.open('GET','https://restcountries.com/v3.1/all');
 xhr.onload=function(){
 let users=JSON.parse(xhr.responseText);
 let userNames=[];
+let countryName=[];
 for(let  user of users){
     userNames.push(user['population']);
+    countryName.push(user['name'])
     console.log(userNames);
+    console.log(countryName)
 
 }
 };
